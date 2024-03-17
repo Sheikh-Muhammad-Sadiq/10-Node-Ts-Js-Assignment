5af5fbde385d3cdf194264387f940866d1162352
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:44:04 -0700

    Add files via upload
    Q 10 Node.js Assignments

---
+++ Q10.js
@@ -0,0 +1,15 @@
+"use strict";
+// Adding comments: choose two of the programs you've written, and add at least one comment to each.
+//  If you don't have anything specific to write because your programs are too simple at this point, 
+//  just add your name and the current date at the top of each program File. Then write one sentence 
+//  describing what the program does.  
+let a = 12;
+let b = 24;
+// Addition
+console.log(`${a}+${b} = ${a + b}`);
+//Subtraction
+console.log(`${a}-${b} = ${a - b}`);
+// Multiplication
+console.log(`${a}*${b} = ${a * b}`);
+// Division
+console.log(`${a}/${b} = ${a / b}`);
